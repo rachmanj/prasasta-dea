c7281a828863202d2f5080d65a1a74e74e98e62f
Rachman J <rachmanj@gmail.com>
2026-08-20 01:49:20 +0000

    feat: tambah akun 5900 Beban Peralatan Kantor + sinkron COA seeder dgn prod
    - Tambah 5900 Beban Peralatan Kantor (Monitor+UPS)
    - Tambah 2200 Hutang Pinjaman, 4400 Pendapatan Bunga, 5800 Beban Pelatihan ke seeder
    - Rename 1011 Bank BCA -> Giro Mandiri, hapus 1012 Bank BRI (prod hanya punya Giro Mandiri)
    - Import transaksi Februari 2026 (72 record, balanced)

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -12,11 +12,11 @@ class AccountSeeder extends Seeder
         $accounts = [
             // Aset
             ['code' => '1000', 'name' => 'Kas', 'type' => 'asset', 'is_bank' => false],
-            ['code' => '1011', 'name' => 'Bank BCA', 'type' => 'asset', 'is_bank' => true, 'bank_name' => 'BCA'],
-            ['code' => '1012', 'name' => 'Bank BRI', 'type' => 'asset', 'is_bank' => true, 'bank_name' => 'BRI'],
+            ['code' => '1011', 'name' => 'Giro Mandiri', 'type' => 'asset', 'is_bank' => true, 'bank_name' => 'Mandiri'],
             ['code' => '1100', 'name' => 'Piutang Usaha', 'type' => 'asset', 'is_bank' => false],
             // Kewajiban
             ['code' => '2100', 'name' => 'Hutang Usaha', 'type' => 'liability', 'is_bank' => false],
+            ['code' => '2200', 'name' => 'Hutang Pinjaman', 'type' => 'liability', 'is_bank' => false],
             // Ekuitas
             ['code' => '3000', 'name' => 'Ekuitas Yayasan', 'type' => 'equity', 'is_bank' => false],
             ['code' => '3100', 'name' => 'Saldo Awal / Laba Ditahan', 'type' => 'equity', 'is_bank' => false],
@@ -24,6 +24,7 @@ class AccountSeeder extends Seeder
             ['code' => '4100', 'name' => 'Pendapatan Kursus / Pelatihan', 'type' => 'revenue', 'is_bank' => false],
             ['code' => '4200', 'name' => 'Pendapatan Donasi', 'type' => 'revenue', 'is_bank' => false],
             ['code' => '4300', 'name' => 'Pendapatan Hibah / Bantuan', 'type' => 'revenue', 'is_bank' => false],
+            ['code' => '4400', 'name' => 'Pendapatan Bunga', 'type' => 'revenue', 'is_bank' => false],
             // Beban
             ['code' => '5100', 'name' => 'Beban Honor Pengajar', 'type' => 'expense', 'is_bank' => false],
             ['code' => '5200', 'name' => 'Beban Gaji Staf', 'type' => 'expense', 'is_bank' => false],
@@ -32,6 +33,8 @@ class AccountSeeder extends Seeder
             ['code' => '5500', 'name' => 'Beban ATK & Konsumsi', 'type' => 'expense', 'is_bank' => false],
             ['code' => '5600', 'name' => 'Beban Pemeliharaan', 'type' => 'expense', 'is_bank' => false],
             ['code' => '5700', 'name' => 'Beban Operasional Lainnya', 'type' => 'expense', 'is_bank' => false],
+            ['code' => '5800', 'name' => 'Beban Pelatihan', 'type' => 'expense', 'is_bank' => false],
+            ['code' => '5900', 'name' => 'Beban Peralatan Kantor', 'type' => 'expense', 'is_bank' => false],
         ];
 
         foreach ($accounts as $account) {
